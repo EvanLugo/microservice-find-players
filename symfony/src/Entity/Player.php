dbfe4aafd3d3b5c6871e54a384499821a8a98d29
evan lugo <evan.lugo.trejo@gmail.com>
2022-10-25 07:18:43 -0500

    importing Doctrine and Symfony maker bundle yml files

---
+++ symfony/src/Entity/Player.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlayerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PlayerRepository::class)
+ */
+class Player
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $platform;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $account;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPlatform(): ?string
+    {
+        return $this->platform;
+    }
+
+    public function setPlatform(string $platform): self
+    {
+        $this->platform = $platform;
+
+        return $this;
+    }
+
+    public function getAccount(): ?string
+    {
+        return $this->account;
+    }
+
+    public function setAccount(string $account): self
+    {
+        $this->account = $account;
+
+        return $this;
+    }
+}
